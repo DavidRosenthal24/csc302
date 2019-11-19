8355c02ff5f48ea51dd0c70e77030fb54ad42ef8
DavidRosenthal24 <57142653+DavidRosenthal24@users.noreply.github.com>
2019-11-19 09:47:22 -0500

    Create sniffer.py

---
+++ sniffer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+from scapy.all import *
+
+def print_pkt():
+  pkt.show()
+pkt = sniff(filter = 'icmp', timeout = 15, count = 15)
+
+for packet in pkt:
+  print("Gotcha")
+  print(packet[IP].src
